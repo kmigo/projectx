c8ee1bb4196b5136c9f3b3c222e5397af2de996f
Patrick Soares de souza silva <patrick.soares@iholdbank.digital>
2023-10-03 11:29:43 -0300

    validate pincode

---
+++ micro_app_onboarding/lib/app/pages/validate_pincode/validate_pincode_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ValidatePincodePage extends StatefulWidget {
+  const ValidatePincodePage({super.key});
+
+  @override
+  State<ValidatePincodePage> createState() => _ValidatePincodePageState();
+}
+
+class _ValidatePincodePageState extends State<ValidatePincodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
